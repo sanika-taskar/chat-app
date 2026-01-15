03979ca8e52d018cd6aef5996d4020cad54a84f2
Sanika Taskar <sanikataskar69@gmail.com>
2026-01-15 22:32:46 +0530

    server.js updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,19 +3,19 @@ import { Navigate, Route, Routes } from 'react-router-dom'
 import HomePage from './pages/HomePage'
 import LoginPage from './pages/LoginPage'
 import ProfilePage from './pages/ProfilePage'
-import {Toaster} from "react-hot-toast";
+import { Toaster } from "react-hot-toast";
 import { AuthContext } from '../context/AuthContext'
 
 const App = () => {
 
-  const {authUser}=useContext(AuthContext);
+  const { authUser } = useContext(AuthContext);
   return (
-    <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain">
+    <div className="bg-[url('/bgImage.svg')] bg-contain">
       <Toaster></Toaster>
       <Routes>
-        <Route path='/' element={authUser ? <HomePage/> : <Navigate to="/login"/>} />
-        <Route path='/login' element={!authUser ?<LoginPage/> : <Navigate to="/"/>} />
-        <Route path='/profile' element={authUser ? <ProfilePage/> : <Navigate to="/login"/>} />
+        <Route path='/' element={authUser ? <HomePage /> : <Navigate to="/login" />} />
+        <Route path='/login' element={!authUser ? <LoginPage /> : <Navigate to="/" />} />
+        <Route path='/profile' element={authUser ? <ProfilePage /> : <Navigate to="/login" />} />
       </Routes>
     </div>
   )
--- server/server.js
+++ server/server.js
@@ -5,53 +5,57 @@ import http from 'http';
 import { connectDb } from './lib/db.js';
 import userRouter from './routes/userRoutes.js';
 import messageRouter from './routes/messageRoutes.js';
-import {Server} from "socket.io";
+import { Server } from "socket.io";
 import user from './models/User.js';
 
 const app = express();
-const server=http.createServer(app);
+const server = http.createServer(app);
 
 //initialize socket.io server
-export const io = new Server(server,{
-    cors:{origin : "*"}
+export const io = new Server(server, {
+    cors: { origin: "*" }
 })
 
 //store online users
-export const userSocketMap ={};
+export const userSocketMap = {};
 
 //socket.io connection handler
-io.on("connection",(socket)=>{
-    const userId=socket.handshake.query.userId;
-    console.log("User connected",userId);
+io.on("connection", (socket) => {
+    const userId = socket.handshake.query.userId;
+    console.log("User connected", userId);
 
-    if(userId){
-        userSocketMap[userId]=socket.id;
+    if (userId) {
+        userSocketMap[userId] = socket.id;
     }
-    io.emit("getOnlineUsers",Object.keys(userSocketMap));
+    io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
-    socket.on("disconnect",()=>{
-        console.log("user disconnected",userId);
+    socket.on("disconnect", () => {
+        console.log("user disconnected", userId);
         delete userSocketMap[userId];
-        io.emit("getOnlineUsers",Object.keys(userSocketMap));
+        io.emit("getOnlineUsers", Object.keys(userSocketMap));
     })
 
 })
 //middleware setup
 
-app.use(express.json({limit:"10mb"}));
+app.use(express.json({ limit: "10mb" }));
 app.use(cors());
 
 //route setup
-app.use("/api/status",(req,res)=> res.send("server is live"));
-app.use("/api/auth",userRouter);
-app.use("/api/messages",messageRouter)
+app.use("/api/status", (req, res) => res.send("server is live"));
+app.use("/api/auth", userRouter);
+app.use("/api/messages", messageRouter)
 
 //connect to mongodb
 
 await connectDb();
 
+if (process.env.NODE_ENV !== "production") {
+    const PORT = process.env.PORT || 5000;
+    server.listen(PORT, () => {
+        console.log("server is running on the port " + PORT);
+    });
+}
 
-const PORT = process.env.PORT || 5000;
-server.listen(PORT,()=>{
-    console.log("server is running on the port "+ PORT);
-});
+//export server for vercel
+export default server
